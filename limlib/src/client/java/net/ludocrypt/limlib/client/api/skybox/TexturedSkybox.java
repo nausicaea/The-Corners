eebb33d68c356a489932e2164b6ad9479e1efc31
Eleanor Young <developer@nausicaea.net>
2025-08-23 23:34:50 +0200

    refactor(limlib)!: move TexturedSkybox into the client source set

--- limlib/src/client/java/net/ludocrypt/limlib/client/api/skybox/TexturedSkybox.java
+++ limlib/src/client/java/net/ludocrypt/limlib/client/api/skybox/TexturedSkybox.java
@@ -1,5 +1,6 @@
-package net.ludocrypt.limlib.api.skybox;
+package net.ludocrypt.limlib.client.api.skybox;
 
+import net.ludocrypt.limlib.api.skybox.Skybox;
 import org.joml.Matrix4f;
 import org.quiltmc.loader.api.minecraft.ClientOnly;
 
