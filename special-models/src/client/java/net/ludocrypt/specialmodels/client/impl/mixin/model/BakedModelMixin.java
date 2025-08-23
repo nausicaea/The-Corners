665b74d2fa71101d410c35344b34ac07015c31dc
Eleanor Young <developer@nausicaea.net>
2025-08-23 22:44:20 +0200

    refactor(special-models): fix import paths in BakedModelMixin

--- special-models/src/client/java/net/ludocrypt/specialmodels/client/impl/mixin/model/BakedModelMixin.java
+++ special-models/src/client/java/net/ludocrypt/specialmodels/client/impl/mixin/model/BakedModelMixin.java
@@ -8,8 +8,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import com.google.common.collect.Lists;
 import com.mojang.datafixers.util.Pair;
 
-import net.ludocrypt.specialmodels.api.SpecialModelRenderer;
-import net.ludocrypt.specialmodels.impl.access.BakedModelAccess;
+import net.ludocrypt.specialmodels.client.api.SpecialModelRenderer;
+import net.ludocrypt.specialmodels.client.impl.access.BakedModelAccess;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.render.model.BakedModel;
 
