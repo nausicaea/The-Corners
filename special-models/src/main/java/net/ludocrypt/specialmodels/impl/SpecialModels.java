faffd13fdf1b3ce23e295c40bf262dcd197dc62d
Eleanor Young <developer@nausicaea.net>
2025-08-23 22:09:53 +0200

    refactor(special-models): remove client-only code from the mod initializer

--- special-models/src/main/java/net/ludocrypt/specialmodels/impl/SpecialModels.java
+++ special-models/src/main/java/net/ludocrypt/specialmodels/impl/SpecialModels.java
@@ -1,29 +1,14 @@
 package net.ludocrypt.specialmodels.impl;
 
-import java.util.Map;
-
-import org.quiltmc.loader.api.ModContainer;
-import org.quiltmc.loader.api.minecraft.ClientOnly;
-import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.collect.Maps;
-
-import net.ludocrypt.specialmodels.api.SpecialModelRenderer;
-import net.ludocrypt.specialmodels.api.TexturedSpecialModelRenderer;
-import net.minecraft.client.gl.ShaderProgram;
-
 public class SpecialModels implements ModInitializer {
-
 	public static final Logger LOGGER = LoggerFactory.getLogger("Special-Models");
 
-	@ClientOnly
-	public static final Map<SpecialModelRenderer, ShaderProgram> LOADED_SHADERS = Maps.newHashMap();
-
 	@Override
-	public void onInitialize(ModContainer mod) {
-		TexturedSpecialModelRenderer.init();
-	}
+	public void onInitialize() {
 
+	}
 }
