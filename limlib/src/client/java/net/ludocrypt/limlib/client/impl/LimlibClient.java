90199ac2e53b57c4fa77e36320276d5b832af00e
Eleanor Young <developer@nausicaea.net>
2025-08-23 23:47:27 +0200

    refactor(limlib)!: move registration code into the client source set

--- limlib/src/client/java/net/ludocrypt/limlib/client/impl/LimlibClient.java
+++ limlib/src/client/java/net/ludocrypt/limlib/client/impl/LimlibClient.java
@@ -1,17 +1,37 @@
 package net.ludocrypt.limlib.client.impl;
 
-import org.quiltmc.loader.api.ModContainer;
-import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
-import org.quiltmc.qsl.resource.loader.api.ResourceLoader;
-
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.ludocrypt.limlib.client.api.effects.sky.DimensionEffects;
+import net.ludocrypt.limlib.client.api.effects.sky.EmptyDimensionEffects;
+import net.ludocrypt.limlib.client.api.effects.sky.StaticDimensionEffects;
+import net.ludocrypt.limlib.client.api.effects.sound.distortion.DistortionEffect;
+import net.ludocrypt.limlib.client.api.effects.sound.distortion.StaticDistortionEffect;
+import net.ludocrypt.limlib.client.api.effects.sound.reverb.ReverbEffect;
+import net.ludocrypt.limlib.client.api.effects.sound.reverb.StaticReverbEffect;
+import net.ludocrypt.limlib.client.api.skybox.EmptySkybox;
+import net.ludocrypt.limlib.client.api.skybox.Skybox;
+import net.ludocrypt.limlib.client.api.skybox.TexturedSkybox;
 import net.ludocrypt.limlib.client.impl.shader.PostProcesserManager;
+import net.minecraft.registry.Registry;
 import net.minecraft.resource.ResourceType;
+import net.minecraft.util.Identifier;
 
 public class LimlibClient implements ClientModInitializer {
-
 	@Override
-	public void onInitializeClient(ModContainer mod) {
-		ResourceLoader.get(ResourceType.CLIENT_RESOURCES).registerReloader(PostProcesserManager.INSTANCE);
+	public void onInitializeClient() {
+		Registry.register(ReverbEffect.REVERB_EFFECT_CODEC, new Identifier("limlib", "static"), StaticReverbEffect.CODEC);
+		Registry
+			.register(DistortionEffect.DISTORTION_EFFECT_CODEC, new Identifier("limlib", "static"),
+				StaticDistortionEffect.CODEC);
+		Registry
+			.register(DimensionEffects.DIMENSION_EFFECTS_CODEC, new Identifier("limlib", "static"),
+				StaticDimensionEffects.CODEC);
+		Registry
+			.register(DimensionEffects.DIMENSION_EFFECTS_CODEC, new Identifier("limlib", "empty"),
+				EmptyDimensionEffects.CODEC);
+		Registry.register(Skybox.SKYBOX_CODEC, new Identifier("limlib", "empty"), EmptySkybox.CODEC);
+		Registry.register(Skybox.SKYBOX_CODEC, new Identifier("limlib", "textured"), TexturedSkybox.CODEC);
+		ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(PostProcesserManager.INSTANCE);
 	}
-
 }
--- limlib/src/main/java/net/ludocrypt/limlib/impl/Limlib.java
+++ limlib/src/main/java/net/ludocrypt/limlib/impl/Limlib.java
@@ -1,8 +1,7 @@
 package net.ludocrypt.limlib.impl;
 
-import org.quiltmc.loader.api.ModContainer;
-import org.quiltmc.loader.api.QuiltLoader;
-import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,16 +10,6 @@ import net.ludocrypt.limlib.api.LimlibWorld;
 import net.ludocrypt.limlib.api.effects.post.EmptyPostEffect;
 import net.ludocrypt.limlib.api.effects.post.PostEffect;
 import net.ludocrypt.limlib.api.effects.post.StaticPostEffect;
-import net.ludocrypt.limlib.api.effects.sky.DimensionEffects;
-import net.ludocrypt.limlib.api.effects.sky.EmptyDimensionEffects;
-import net.ludocrypt.limlib.api.effects.sky.StaticDimensionEffects;
-import net.ludocrypt.limlib.api.effects.sound.distortion.DistortionEffect;
-import net.ludocrypt.limlib.api.effects.sound.distortion.StaticDistortionEffect;
-import net.ludocrypt.limlib.api.effects.sound.reverb.ReverbEffect;
-import net.ludocrypt.limlib.api.effects.sound.reverb.StaticReverbEffect;
-import net.ludocrypt.limlib.api.skybox.EmptySkybox;
-import net.ludocrypt.limlib.api.skybox.Skybox;
-import net.ludocrypt.limlib.api.skybox.TexturedSkybox;
 import net.ludocrypt.limlib.impl.debug.DebugNbtChunkGenerator;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -31,26 +20,14 @@ public class Limlib implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("Limlib");
 
 	@Override
-	public void onInitialize(ModContainer mod) {
+	public void onInitialize() {
 		LimlibWorld.load();
-		Registry.register(ReverbEffect.REVERB_EFFECT_CODEC, new Identifier("limlib", "static"), StaticReverbEffect.CODEC);
-		Registry
-			.register(DistortionEffect.DISTORTION_EFFECT_CODEC, new Identifier("limlib", "static"),
-				StaticDistortionEffect.CODEC);
-		Registry
-			.register(DimensionEffects.DIMENSION_EFFECTS_CODEC, new Identifier("limlib", "static"),
-				StaticDimensionEffects.CODEC);
-		Registry
-			.register(DimensionEffects.DIMENSION_EFFECTS_CODEC, new Identifier("limlib", "empty"),
-				EmptyDimensionEffects.CODEC);
 		Registry.register(PostEffect.POST_EFFECT_CODEC, new Identifier("limlib", "static"), StaticPostEffect.CODEC);
 		Registry.register(PostEffect.POST_EFFECT_CODEC, new Identifier("limlib", "empty"), EmptyPostEffect.CODEC);
-		Registry.register(Skybox.SKYBOX_CODEC, new Identifier("limlib", "empty"), EmptySkybox.CODEC);
-		Registry.register(Skybox.SKYBOX_CODEC, new Identifier("limlib", "textured"), TexturedSkybox.CODEC);
 		Registry
 			.register(Registries.CHUNK_GENERATOR, new Identifier("limlib", "debug_nbt_chunk_generator"),
 				DebugNbtChunkGenerator.CODEC);
-		QuiltLoader
+		FabricLoader.getInstance()
 			.getEntrypoints(LimlibRegistrar.ENTRYPOINT_KEY, LimlibRegistrar.class)
 			.forEach(LimlibRegistrar::registerHooks);
 	}
