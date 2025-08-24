0931f62ea783c753d407c95280f3211ae0ba1bb5
Eleanor Young <developer@nausicaea.net>
2025-08-24 11:13:36 +0200

    Revert "refactor(limlib)!: migrate QuiltLoader to FabricLoader API"
    This reverts commit e8f81bdd2898d790697e546e639b3ca658fe96c9.

--- limlib/src/main/java/net/ludocrypt/limlib/impl/bridge/IrisBridge.java
+++ limlib/src/main/java/net/ludocrypt/limlib/impl/bridge/IrisBridge.java
@@ -1,13 +1,13 @@
 package net.ludocrypt.limlib.impl.bridge;
 
-import net.fabricmc.loader.api.FabricLoader;
-
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
+import org.quiltmc.loader.api.QuiltLoader;
+
 public class IrisBridge {
 
-	public static final boolean IRIS_LOADED = FabricLoader.getInstance().isModLoaded("iris");
+	public static final boolean IRIS_LOADED = QuiltLoader.isModLoaded("iris");
 
 	public static boolean areShadersInUse() {
 
