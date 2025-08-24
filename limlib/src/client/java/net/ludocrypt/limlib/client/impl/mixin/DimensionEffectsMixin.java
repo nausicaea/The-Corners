fe08fd461335c4e3671d019eb46d44196d7bdfd2
Eleanor Young <developer@nausicaea.net>
2025-08-24 14:43:19 +0200

    refactor(limlib): fix DimensionVisualEffectsMixin.java

--- limlib/src/client/java/net/ludocrypt/limlib/client/impl/mixin/DimensionEffectsMixin.java
+++ limlib/src/client/java/net/ludocrypt/limlib/client/impl/mixin/DimensionEffectsMixin.java
@@ -2,26 +2,25 @@ package net.ludocrypt.limlib.client.impl.mixin;
 
 import java.util.Optional;
 
+import net.ludocrypt.limlib.client.api.effects.sky.DimensionEffects;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.ludocrypt.limlib.api.effects.LookupGrabber;
-import net.ludocrypt.limlib.client.api.effects.sky.DimensionEffects;
-import net.minecraft.client.render.DimensionVisualEffects;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.world.dimension.DimensionType;
 
-@Mixin(DimensionVisualEffects.class)
-public class DimensionVisualEffectsMixin {
+@Mixin(net.minecraft.client.render.DimensionEffects.class)
+public class DimensionEffectsMixin {
 
 	@Inject(method = "byDimensionType", at = @At("HEAD"), cancellable = true)
 	private static void limlib$byDimensionType(DimensionType dimensionType,
-			CallbackInfoReturnable<DimensionVisualEffects> ci) {
+			CallbackInfoReturnable<net.minecraft.client.render.DimensionEffects> ci) {
 		Optional<DimensionEffects> dimensionEffects = LookupGrabber
 			.snatch(DimensionEffects.MIXIN_WORLD_LOOKUP.get(),
-				RegistryKey.of(DimensionEffects.DIMENSION_EFFECTS_KEY, dimensionType.effectsLocation()));
+				RegistryKey.of(DimensionEffects.DIMENSION_EFFECTS_KEY, dimensionType.effects()));
 
 		if (dimensionEffects.isPresent()) {
 			ci.setReturnValue(dimensionEffects.get().getDimensionEffects());
