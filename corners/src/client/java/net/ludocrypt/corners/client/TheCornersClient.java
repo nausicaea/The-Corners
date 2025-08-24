00d64ef2d2106f2cdc3cc1d178d825dbc8cb012c
Eleanor Young <developer@nausicaea.net>
2025-08-24 11:11:19 +0200

    refactor(corners): add missing imports in TheCornersClient

--- corners/src/client/java/net/ludocrypt/corners/client/TheCornersClient.java
+++ corners/src/client/java/net/ludocrypt/corners/client/TheCornersClient.java
@@ -1,29 +1,42 @@
 package net.ludocrypt.corners.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.fabricmc.fabric.impl.client.rendering.EntityRendererRegistryImpl;
+import net.ludocrypt.corners.client.render.CornerBoatEntityRenderer;
+import net.ludocrypt.corners.entity.CornerBoatEntity;
+import net.ludocrypt.corners.init.CornerBlocks;
+import net.ludocrypt.corners.init.CornerEntities;
+import net.ludocrypt.corners.packet.ServerToClientPackets;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.entity.PaintingEntityRenderer;
+import net.minecraft.client.render.entity.model.BoatEntityModel;
+import net.minecraft.client.render.entity.model.ChestBoatEntityModel;
 
 public class TheCornersClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ServerToClientPackets.manageServerToClientPackets();
-        BlockRenderLayerMap
-                .put(RenderLayer.getCutout(), CornerBlocks.SNOWY_GLASS_PANE, CornerBlocks.SNOWY_GLASS,
+        BlockRenderLayerMap.INSTANCE
+                .putBlocks(RenderLayer.getCutout(), CornerBlocks.SNOWY_GLASS_PANE, CornerBlocks.SNOWY_GLASS,
                         CornerBlocks.SNOWY_GLASS_SLAB, CornerBlocks.GAIA_DOOR, CornerBlocks.GAIA_TRAPDOOR, CornerBlocks.GAIA_SAPLING,
                         CornerBlocks.POTTED_GAIA_SAPLING);
         EntityRendererRegistryImpl.register(CornerEntities.DIMENSIONAL_PAINTING_ENTITY, PaintingEntityRenderer::new);
 //		EntityRendererRegistryImpl.register(CornerEntities.CORVUS_ENTITY, CorvusEntityRenderer::new);
 //		EntityModelLayerRegistry.registerModelLayer(CorvusEntityModel.LAYER_LOCATION, () -> CorvusEntityModel.createBodyLayer());
         EntityRendererRegistry
-                .register(CornerBoat.GAIA.entityType(false),
-                        context -> new CornerBoatEntityRenderer(context, false, CornerBoat.GAIA));
+                .register(CornerBoatEntity.CornerBoat.GAIA.entityType(false),
+                        context -> new CornerBoatEntityRenderer(context, false, CornerBoatEntity.CornerBoat.GAIA));
         EntityModelLayerRegistry
-                .registerModelLayer(CornerBoatEntityRenderer.getModelLayer(CornerBoat.GAIA, false),
+                .registerModelLayer(CornerBoatEntityRenderer.getModelLayer(CornerBoatEntity.CornerBoat.GAIA, false),
                         () -> BoatEntityModel.getTexturedModelData());
         EntityRendererRegistry
-                .register(CornerBoat.GAIA.entityType(true),
-                        context -> new CornerBoatEntityRenderer(context, true, CornerBoat.GAIA));
+                .register(CornerBoatEntity.CornerBoat.GAIA.entityType(true),
+                        context -> new CornerBoatEntityRenderer(context, true, CornerBoatEntity.CornerBoat.GAIA));
         EntityModelLayerRegistry
-                .registerModelLayer(CornerBoatEntityRenderer.getModelLayer(CornerBoat.GAIA, true),
+                .registerModelLayer(CornerBoatEntityRenderer.getModelLayer(CornerBoatEntity.CornerBoat.GAIA, true),
                         () -> ChestBoatEntityModel.getTexturedModelData());
     }
 }
