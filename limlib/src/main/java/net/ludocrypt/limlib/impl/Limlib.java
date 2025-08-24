c20acadf73784f12644ea05f4413765c7efd0860
Eleanor Young <developer@nausicaea.net>
2025-08-24 11:13:39 +0200

    Revert "refactor(limlib)!: move registration code into the client source set"
    This reverts commit 90199ac2e53b57c4fa77e36320276d5b832af00e.

--- limlib/src/main/java/net/ludocrypt/limlib/impl/Limlib.java
+++ limlib/src/main/java/net/ludocrypt/limlib/impl/Limlib.java
@@ -1,7 +1,8 @@
 package net.ludocrypt.limlib.impl;
 
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.loader.api.QuiltLoader;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +11,16 @@ import net.ludocrypt.limlib.api.LimlibWorld;
 import net.ludocrypt.limlib.api.effects.post.EmptyPostEffect;
 import net.ludocrypt.limlib.api.effects.post.PostEffect;
 import net.ludocrypt.limlib.api.effects.post.StaticPostEffect;
+import net.ludocrypt.limlib.api.effects.sky.DimensionEffects;
+import net.ludocrypt.limlib.api.effects.sky.EmptyDimensionEffects;
+import net.ludocrypt.limlib.api.effects.sky.StaticDimensionEffects;
+import net.ludocrypt.limlib.api.effects.sound.distortion.DistortionEffect;
+import net.ludocrypt.limlib.api.effects.sound.distortion.StaticDistortionEffect;
+import net.ludocrypt.limlib.api.effects.sound.reverb.ReverbEffect;
+import net.ludocrypt.limlib.api.effects.sound.reverb.StaticReverbEffect;
+import net.ludocrypt.limlib.api.skybox.EmptySkybox;
+import net.ludocrypt.limlib.api.skybox.Skybox;
+import net.ludocrypt.limlib.api.skybox.TexturedSkybox;
 import net.ludocrypt.limlib.impl.debug.DebugNbtChunkGenerator;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -20,14 +31,26 @@ public class Limlib implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("Limlib");
 
 	@Override
-	public void onInitialize() {
+	public void onInitialize(ModContainer mod) {
 		LimlibWorld.load();
+		Registry.register(ReverbEffect.REVERB_EFFECT_CODEC, new Identifier("limlib", "static"), StaticReverbEffect.CODEC);
+		Registry
+			.register(DistortionEffect.DISTORTION_EFFECT_CODEC, new Identifier("limlib", "static"),
+				StaticDistortionEffect.CODEC);
+		Registry
+			.register(DimensionEffects.DIMENSION_EFFECTS_CODEC, new Identifier("limlib", "static"),
+				StaticDimensionEffects.CODEC);
+		Registry
+			.register(DimensionEffects.DIMENSION_EFFECTS_CODEC, new Identifier("limlib", "empty"),
+				EmptyDimensionEffects.CODEC);
 		Registry.register(PostEffect.POST_EFFECT_CODEC, new Identifier("limlib", "static"), StaticPostEffect.CODEC);
 		Registry.register(PostEffect.POST_EFFECT_CODEC, new Identifier("limlib", "empty"), EmptyPostEffect.CODEC);
+		Registry.register(Skybox.SKYBOX_CODEC, new Identifier("limlib", "empty"), EmptySkybox.CODEC);
+		Registry.register(Skybox.SKYBOX_CODEC, new Identifier("limlib", "textured"), TexturedSkybox.CODEC);
 		Registry
 			.register(Registries.CHUNK_GENERATOR, new Identifier("limlib", "debug_nbt_chunk_generator"),
 				DebugNbtChunkGenerator.CODEC);
-		FabricLoader.getInstance()
+		QuiltLoader
 			.getEntrypoints(LimlibRegistrar.ENTRYPOINT_KEY, LimlibRegistrar.class)
 			.forEach(LimlibRegistrar::registerHooks);
 	}
