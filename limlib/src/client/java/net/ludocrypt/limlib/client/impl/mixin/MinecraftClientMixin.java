47933b87f0bd87757568b90a05786cf890d5b235
Eleanor Young <developer@nausicaea.net>
2025-08-24 14:46:11 +0200

    refactor(limlib): fix MinecraftClientMixin

--- limlib/src/client/java/net/ludocrypt/limlib/client/impl/mixin/MinecraftClientMixin.java
+++ limlib/src/client/java/net/ludocrypt/limlib/client/impl/mixin/MinecraftClientMixin.java
@@ -2,6 +2,7 @@ package net.ludocrypt.limlib.client.impl.mixin;
 
 import java.util.Optional;
 
+import net.minecraft.client.util.Window;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -9,8 +10,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import com.mojang.blaze3d.glfw.Window;
 
 import net.ludocrypt.limlib.api.effects.LookupGrabber;
 import net.ludocrypt.limlib.client.api.effects.sound.SoundEffects;
@@ -34,12 +33,12 @@ public class MinecraftClientMixin {
 	@Shadow
 	private Window window;
 
-	@Inject(method = "getMusic", at = @At("HEAD"), cancellable = true)
+	@Inject(method = "getMusicType()Lnet/minecraft/sound/MusicSound;", at = @At("HEAD"), cancellable = true)
 	private void limlib$getMusic(CallbackInfoReturnable<MusicSound> ci) {
 
 		if (this.player != null) {
 			Optional<SoundEffects> soundEffects = LookupGrabber
-				.snatch(world.getRegistryManager().getLookup(SoundEffects.SOUND_EFFECTS_KEY).get(),
+				.snatch(world.getRegistryManager().getOptionalWrapper(SoundEffects.SOUND_EFFECTS_KEY).get(),
 					RegistryKey.of(SoundEffects.SOUND_EFFECTS_KEY, world.getRegistryKey().getValue()));
 
 			if (soundEffects.isPresent()) {
