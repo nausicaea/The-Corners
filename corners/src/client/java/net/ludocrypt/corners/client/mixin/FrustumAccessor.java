0805ecc3d4a84fc51e708c4f1d76107cbdd4503f
Eleanor Young <developer@nausicaea.net>
2025-08-24 21:29:57 +0200

    refactor(corners): fix FrustumAccessor

--- corners/src/client/java/net/ludocrypt/corners/client/mixin/FrustumAccessor.java
+++ corners/src/client/java/net/ludocrypt/corners/client/mixin/FrustumAccessor.java
@@ -7,8 +7,6 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(Frustum.class)
 public interface FrustumAccessor {
-
 	@Accessor
-	FrustumIntersection getIntersection();
-
+	FrustumIntersection getFrustumIntersection();
 }
