e746c0dbb40d8f35ad35e6aca74f632e1f57ca1f
Eleanor Young <developer@nausicaea.net>
2025-08-26 16:43:30 +0200

    refactor(limlib): refactor SoundSystemMixin to deal with warnings and code quality suggestions

--- limlib/src/client/java/net/ludocrypt/limlib/client/impl/mixin/SoundSystemMixin.java
+++ limlib/src/client/java/net/ludocrypt/limlib/client/impl/mixin/SoundSystemMixin.java
@@ -1,10 +1,9 @@
 package net.ludocrypt.limlib.client.impl.mixin;
 
-import java.util.Iterator;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,7 +12,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import com.google.common.collect.Multimap;
 
@@ -21,13 +19,10 @@ import net.ludocrypt.limlib.client.api.effects.sound.distortion.DistortionFilter
 import net.ludocrypt.limlib.client.api.effects.sound.reverb.ReverbFilter;
 import net.ludocrypt.limlib.impl.access.SoundSystemAccess;
 import net.minecraft.client.sound.Channel;
-import net.minecraft.client.sound.Sound;
 import net.minecraft.client.sound.SoundInstance;
 import net.minecraft.client.sound.SoundSystem;
-import net.minecraft.client.sound.WeightedSoundSet;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.Vec3d;
 
 @Mixin(SoundSystem.class)
 public abstract class SoundSystemMixin implements SoundSystemAccess {
@@ -41,12 +36,11 @@ public abstract class SoundSystemMixin implements SoundSystemAccess {
 	private Multimap<SoundCategory, SoundInstance> sounds;
 
 	@Override
-	public void stopSoundsAtPosition(double x, double y, double z, @Nullable Identifier id,
-			@Nullable SoundCategory category) {
+	public void limlib$stopSoundsAtPosition(double x, double y, double z, @Nullable Identifier id,
+											@Nullable SoundCategory category) {
 		Consumer<SoundInstance> consumer = (soundInstance) -> {
 
-			if ((id != null ? soundInstance.getId().equals(id)
-					: true) && (soundInstance.getX() == x) && (soundInstance.getY() == y) && (soundInstance.getZ() == z)) {
+			if ((id == null || soundInstance.getId().equals(id)) && (soundInstance.getX() == x) && (soundInstance.getY() == y) && (soundInstance.getZ() == z)) {
 				this.stop(soundInstance);
 			}
 
@@ -60,26 +54,24 @@ public abstract class SoundSystemMixin implements SoundSystemAccess {
 
 	}
 
-	@Inject(method = "tick()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/GameOptions;getSoundVolume(Lnet/minecraft/sound/SoundCategory;)F", shift = Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD)
-	public void limlib$tick(CallbackInfo ci, Iterator<?> iterator, Map.Entry<?, ?> entry,
-			Channel.SourceManager sourceManager, SoundInstance soundInstance) {
+	@Inject(method = "tick()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/GameOptions;getSoundVolume(Lnet/minecraft/sound/SoundCategory;)F", shift = Shift.BEFORE))
+	public void limlib$tick(CallbackInfo ci,
+							@Local Channel.SourceManager sourceManager, @Local SoundInstance soundInstance) {
 		sourceManager.run(source -> ReverbFilter.update(soundInstance, ((SourceAccessor) source).getPointer()));
 		sourceManager.run(source -> DistortionFilter.update(soundInstance, ((SourceAccessor) source).getPointer()));
 	}
 
-	@Inject(method = "play(Lnet/minecraft/client/sound/SoundInstance;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/sound/Channel$SourceManager;run(Ljava/util/function/Consumer;)V", ordinal = 0, shift = Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
-	public void limlib$play(SoundInstance soundInstance, CallbackInfo ci, WeightedSoundSet weightedSoundSet,
-			Identifier identifier, Sound sound, float f, float g, SoundCategory soundCategory, float h, float i,
-			SoundInstance.AttenuationType attenuationType, boolean bl, Vec3d vec3d, boolean bl3, boolean bl4,
-			CompletableFuture<?> completableFuture, Channel.SourceManager sourceManager) {
+	@Inject(method = "play(Lnet/minecraft/client/sound/SoundInstance;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/sound/Channel$SourceManager;run(Ljava/util/function/Consumer;)V", ordinal = 0, shift = Shift.AFTER))
+	public void limlib$play(SoundInstance soundInstance, CallbackInfo ci,
+							@Local Channel.SourceManager sourceManager) {
 		sourceManager.run(source -> ReverbFilter.update(soundInstance, ((SourceAccessor) source).getPointer()));
 		sourceManager.run(source -> DistortionFilter.update(soundInstance, ((SourceAccessor) source).getPointer()));
 	}
 
 	@Inject(method = "reloadSounds()V", at = @At("TAIL"))
 	public void limlib$reloadSounds(CallbackInfo ci) {
-		ReverbFilter.update();
-		DistortionFilter.update();
+		ReverbFilter.reload();
+		DistortionFilter.reload();
 	}
 
 	@Shadow
