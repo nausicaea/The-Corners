1ef6551e0b43be050ec13d7b426ce04084489b67
Eleanor Young <developer@nausicaea.net>
2025-08-26 16:34:20 +0200

    refactor(limlib): add Identifier access to the server-side Skybox data

--- limlib/src/main/java/net/ludocrypt/limlib/api/skybox/TexturedSkyboxDto.java
+++ limlib/src/main/java/net/ludocrypt/limlib/api/skybox/TexturedSkyboxDto.java
@@ -5,7 +5,13 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.util.Identifier;
 
 public record TexturedSkyboxDto(Identifier identifier) implements SkyboxDto {
+	public static final Identifier ID = new Identifier("limlib", "textured");
 	public static final Codec<TexturedSkyboxDto> CODEC = RecordCodecBuilder.create((instance) -> instance.group(Identifier.CODEC.fieldOf("skybox").stable().forGetter((sky) -> sky.identifier)).apply(instance, instance.stable(TexturedSkyboxDto::new)));
+
+	@Override
+	public Identifier getId() {
+		return ID;
+	}
 
 	@Override
 	public Codec<TexturedSkyboxDto> getCodec() {
