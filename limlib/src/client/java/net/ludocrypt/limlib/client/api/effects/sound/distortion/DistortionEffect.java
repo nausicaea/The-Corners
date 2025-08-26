488ad674f8fc2867465158c441557073b7a32a46
Eleanor Young <developer@nausicaea.net>
2025-08-26 16:40:07 +0200

    refactor(limlib): convert DistortionEffect to an interface, and update DistortionFilter as well as StaticDistortionEffect to match the new client/server separation

--- limlib/src/client/java/net/ludocrypt/limlib/client/api/effects/sound/distortion/DistortionEffect.java
+++ limlib/src/client/java/net/ludocrypt/limlib/client/api/effects/sound/distortion/DistortionEffect.java
@@ -1,35 +1,23 @@
 package net.ludocrypt.limlib.client.api.effects.sound.distortion;
 
-
-import com.mojang.serialization.Codec;
-
+import net.ludocrypt.limlib.api.effects.sound.distortion.DistortionEffectDto;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.sound.SoundInstance;
-import net.minecraft.util.Identifier;
 
 /**
  * A Distortion effect controls
  */
-public abstract class DistortionEffect {
-	public abstract Codec<? extends DistortionEffect> getCodec();
+public interface DistortionEffect {
+	boolean isEnabled(MinecraftClient client, SoundInstance soundInstance);
 
-	/**
-	 * Whether or not a Sound Event should be ignored
-	 *
-	 * @param identifier the Identifier of the Sound Event
-	 */
-	public abstract boolean shouldIgnore(Identifier identifier);
+	float getEdge(MinecraftClient client, SoundInstance soundInstance);
 
-	public abstract boolean isEnabled(MinecraftClient client, SoundInstance soundInstance);
+	float getGain(MinecraftClient client, SoundInstance soundInstance);
 
-	public abstract float getEdge(MinecraftClient client, SoundInstance soundInstance);
+	float getLowpassCutoff(MinecraftClient client, SoundInstance soundInstance);
 
-	public abstract float getGain(MinecraftClient client, SoundInstance soundInstance);
+	float getEQCenter(MinecraftClient client, SoundInstance soundInstance);
 
-	public abstract float getLowpassCutoff(MinecraftClient client, SoundInstance soundInstance);
-
-	public abstract float getEQCenter(MinecraftClient client, SoundInstance soundInstance);
-
-	public abstract float getEQBandWidth(MinecraftClient client, SoundInstance soundInstance);
+	float getEQBandWidth(MinecraftClient client, SoundInstance soundInstance);
 
 }
--- limlib/src/client/java/net/ludocrypt/limlib/client/api/effects/sound/distortion/DistortionFilter.java
+++ limlib/src/client/java/net/ludocrypt/limlib/client/api/effects/sound/distortion/DistortionFilter.java
@@ -1,70 +1,74 @@
 package net.ludocrypt.limlib.client.api.effects.sound.distortion;
 
-import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import net.ludocrypt.limlib.api.effects.sound.SoundEffectsDto;
+import net.ludocrypt.limlib.api.effects.sound.distortion.DistortionEffectDto;
+import net.ludocrypt.limlib.impl.Limlib;
 import net.ludocrypt.limlib.impl.LimlibRegistries;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import net.minecraft.client.world.ClientWorld;
 import org.lwjgl.openal.AL11;
 import org.lwjgl.openal.EXTEfx;
 
 import net.ludocrypt.limlib.api.effects.LookupGrabber;
-import net.ludocrypt.limlib.client.api.effects.sound.SoundEffects;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.sound.SoundInstance;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.util.math.MathHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DistortionFilter {
 
-	public static final Logger LOGGER = LogManager.getLogger("LimLib | Distortion");
+	public static final Logger LOGGER = LoggerFactory.getLogger("%s.%s".formatted(Limlib.LOGGER.getName(), DistortionFilter.class.getSimpleName()));
 
-	public static int id = -1;
-	public static int slot = -1;
+	private static final AtomicInteger id = new AtomicInteger(-1);
+	private static final AtomicInteger slot = new AtomicInteger(-1);
 
-	public static void update() {
-		id = EXTEfx.alGenEffects();
-		slot = EXTEfx.alGenAuxiliaryEffectSlots();
+	public static void reload() {
+		id.set(EXTEfx.alGenEffects());
+		slot.set(EXTEfx.alGenAuxiliaryEffectSlots());
 	}
 
-	public static boolean update(SoundInstance soundInstance, DistortionEffect data) {
-
-		if (id == -1 || slot == -1) {
-			update();
-		}
+	private static boolean update(SoundInstance soundInstance, DistortionEffectDto dto) {
+		id.compareAndSet(-1, EXTEfx.alGenEffects());
+		slot.compareAndSet(-1, EXTEfx.alGenAuxiliaryEffectSlots());
 
 		MinecraftClient client = MinecraftClient.getInstance();
+		DistortionEffect data = DistortionEffectFactories.resolve(dto);
 
 		if (data.isEnabled(client, soundInstance)) {
-			EXTEfx.alAuxiliaryEffectSlotf(slot, EXTEfx.AL_EFFECTSLOT_GAIN, 0);
-			EXTEfx.alEffecti(id, EXTEfx.AL_EFFECT_TYPE, EXTEfx.AL_EFFECT_DISTORTION);
+			int currentId = id.get();
+			int currentSlot = slot.get();
+			EXTEfx.alAuxiliaryEffectSlotf(currentSlot, EXTEfx.AL_EFFECTSLOT_GAIN, 0);
+			EXTEfx.alEffecti(currentId, EXTEfx.AL_EFFECT_TYPE, EXTEfx.AL_EFFECT_DISTORTION);
 			EXTEfx
-				.alEffectf(id, EXTEfx.AL_DISTORTION_EDGE,
+				.alEffectf(currentId, EXTEfx.AL_DISTORTION_EDGE,
 					MathHelper
 						.clamp(data.getEdge(client, soundInstance), EXTEfx.AL_DISTORTION_MIN_EDGE,
 							EXTEfx.AL_DISTORTION_MAX_EDGE));
 			EXTEfx
-				.alEffectf(id, EXTEfx.AL_DISTORTION_GAIN,
+				.alEffectf(currentId, EXTEfx.AL_DISTORTION_GAIN,
 					MathHelper
 						.clamp(data.getGain(client, soundInstance), EXTEfx.AL_DISTORTION_MIN_GAIN,
 							EXTEfx.AL_DISTORTION_MAX_GAIN));
 			EXTEfx
-				.alEffectf(id, EXTEfx.AL_DISTORTION_LOWPASS_CUTOFF,
+				.alEffectf(currentId, EXTEfx.AL_DISTORTION_LOWPASS_CUTOFF,
 					MathHelper
 						.clamp(data.getLowpassCutoff(client, soundInstance), EXTEfx.AL_DISTORTION_MIN_LOWPASS_CUTOFF,
 							EXTEfx.AL_DISTORTION_MAX_LOWPASS_CUTOFF));
 			EXTEfx
-				.alEffectf(id, EXTEfx.AL_DISTORTION_EQCENTER,
+				.alEffectf(currentId, EXTEfx.AL_DISTORTION_EQCENTER,
 					MathHelper
 						.clamp(data.getEQCenter(client, soundInstance), EXTEfx.AL_DISTORTION_MIN_EQCENTER,
 							EXTEfx.AL_DISTORTION_MAX_EQCENTER));
 			EXTEfx
-				.alEffectf(id, EXTEfx.AL_DISTORTION_EQBANDWIDTH,
+				.alEffectf(currentId, EXTEfx.AL_DISTORTION_EQBANDWIDTH,
 					MathHelper
 						.clamp(data.getEQBandWidth(client, soundInstance), EXTEfx.AL_DISTORTION_MIN_EQBANDWIDTH,
 							EXTEfx.AL_DISTORTION_MAX_EQBANDWIDTH));
-			EXTEfx.alAuxiliaryEffectSloti(slot, EXTEfx.AL_EFFECTSLOT_EFFECT, id);
-			EXTEfx.alAuxiliaryEffectSlotf(slot, EXTEfx.AL_EFFECTSLOT_GAIN, 1);
+			EXTEfx.alAuxiliaryEffectSloti(currentSlot, EXTEfx.AL_EFFECTSLOT_EFFECT, currentId);
+			EXTEfx.alAuxiliaryEffectSlotf(currentSlot, EXTEfx.AL_EFFECTSLOT_GAIN, 1);
 
 			return true;
 		}
@@ -74,42 +78,42 @@ public class DistortionFilter {
 
 	public static void update(SoundInstance soundInstance, int sourceID) {
 		MinecraftClient client = MinecraftClient.getInstance();
+		if (client == null) {
+			return;
+		}
+		ClientWorld world = client.world;
+		if (world == null) {
+			return;
+		}
 
-		if (!(client == null || client.world == null)) {
-			Optional<SoundEffects> soundEffects = LookupGrabber
-				.snatch(client.world.getRegistryManager().getOptionalWrapper(LimlibRegistries.SndFx.REGISTRY_KEY).get(),
-					RegistryKey.of(LimlibRegistries.SndFx.REGISTRY_KEY, client.world.getRegistryKey().getValue()));
+		var dynamicRegistries = world.getRegistryManager();
+		var soundEffectsRegistry = dynamicRegistries
+			.getOptionalWrapper(LimlibRegistries.SndFx.REGISTRY_KEY)
+			.orElseThrow(() -> new IllegalStateException("Client: Cannot find sound effects registry (key: %s)".formatted(LimlibRegistries.SndFx.REGISTRY_KEY)));
 
-			if (soundEffects.isPresent()) {
-				Optional<DistortionEffect> distortion = soundEffects.get().getDistortion();
+		var soundInstanceId = soundInstance.getId();
+		var effect = world.getRegistryKey().getValue();
+		LookupGrabber
+			.snatch(soundEffectsRegistry,
+				RegistryKey.of(LimlibRegistries.SndFx.REGISTRY_KEY, effect))
+			.flatMap(SoundEffectsDto::distortion)
+			.filter(d -> !d.shouldIgnore(soundInstanceId))
+			.ifPresent(d -> {
+				for (int i = 0; i < 2; i++) {
+					AL11.alSourcei(sourceID, EXTEfx.AL_DIRECT_FILTER, 0);
+					AL11
+						.alSource3i(sourceID, EXTEfx.AL_AUXILIARY_SEND_FILTER,
+							update(soundInstance, d) ? slot.get() : 0, 0, 0);
+					int error = AL11.alGetError();
 
-				if (distortion.isPresent()) {
-
-					if (!distortion.get().shouldIgnore(soundInstance.getId())) {
-
-						for (int i = 0; i < 2; i++) {
-							AL11.alSourcei(sourceID, EXTEfx.AL_DIRECT_FILTER, 0);
-							AL11
-								.alSource3i(sourceID, EXTEfx.AL_AUXILIARY_SEND_FILTER,
-									update(soundInstance, distortion.get()) ? slot : 0, 0, 0);
-							int error = AL11.alGetError();
-
-							if (error == AL11.AL_NO_ERROR) {
-								break;
-							} else {
-								LOGGER.warn("OpenAl Error {}", error);
-							}
-
-						}
-
+					if (error == AL11.AL_NO_ERROR) {
+						break;
+					} else {
+						LOGGER.warn("OpenAl Error {}", error);
 					}
 
 				}
-
-			}
-
-		}
-
+			});
 	}
 
 }
