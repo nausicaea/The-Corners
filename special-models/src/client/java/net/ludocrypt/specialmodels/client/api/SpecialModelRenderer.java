db03674ab249a91f4fafaff01e0b4d5fba246f2e
Eleanor Young <developer@nausicaea.net>
2025-09-02 13:30:22 +0200

    refactor(special-models): convert SpecialModelRenderer to an interface

--- special-models/src/client/java/net/ludocrypt/specialmodels/client/api/SpecialModelRenderer.java
+++ special-models/src/client/java/net/ludocrypt/specialmodels/client/api/SpecialModelRenderer.java
@@ -1,12 +1,9 @@
 package net.ludocrypt.specialmodels.client.api;
 
 import net.ludocrypt.specialmodels.client.impl.ClientSharedMutableState;
-import net.ludocrypt.specialmodels.client.impl.SpecialModelsClient;
 import org.joml.Matrix4f;
 
-import com.mojang.serialization.Lifecycle;
 
-import net.ludocrypt.specialmodels.impl.mixin.registry.RegistriesAccessor;
 import net.ludocrypt.specialmodels.impl.render.MutableQuad;
 import net.ludocrypt.specialmodels.impl.render.Vec4b;
 import net.minecraft.block.BlockState;
@@ -15,45 +12,31 @@ import net.minecraft.client.render.chunk.ChunkRendererRegion;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.BakedQuad;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 
-public abstract class SpecialModelRenderer {
-
-	public final boolean performOutside;
-
-	public SpecialModelRenderer() {
-		this.performOutside = true;
-	}
-
-	public SpecialModelRenderer(boolean performOutside) {
-		this.performOutside = performOutside;
-	}
-
-	public abstract void setup(MatrixStack matrices, Matrix4f viewMatrix, Matrix4f positionMatrix, float tickDelta,
+public interface SpecialModelRenderer {
+	void setup(MatrixStack matrices, Matrix4f viewMatrix, Matrix4f positionMatrix, float tickDelta,
 			ShaderProgram shader, BlockPos chunkOrigin);
 
-	public MutableQuad modifyQuad(ChunkRendererRegion chunkRenderRegion, BlockPos pos, BlockState state, BakedModel model,
+	default MutableQuad modifyQuad(ChunkRendererRegion chunkRenderRegion, BlockPos pos, BlockState state, BakedModel model,
 			BakedQuad quadIn, long modelSeed, MutableQuad quad) {
 		return quad;
 	}
 
-	public Matrix4f positionMatrix(Matrix4f in) {
+	default Matrix4f positionMatrix(Matrix4f in) {
 		return in;
 	}
 
-	public Matrix4f viewMatrix(Matrix4f in) {
+	default Matrix4f viewMatrix(Matrix4f in) {
 		return in;
 	}
 
-	public Vec4b appendState(ChunkRendererRegion chunkRenderRegion, BlockPos pos, BlockState state, BakedModel model,
+	default Vec4b appendState(ChunkRendererRegion chunkRenderRegion, BlockPos pos, BlockState state, BakedModel model,
 			long modelSeed) {
 		return new Vec4b(0, 0, 0, 0);
 	}
 
-	public ShaderProgram getShaderProgram(MatrixStack matrices, float tickDelta) {
+	default ShaderProgram getShaderProgram(MatrixStack matrices, float tickDelta) {
 		return ClientSharedMutableState.LOADED_SHADERS.get(this);
 	}
 
