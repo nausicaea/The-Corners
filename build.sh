3ab4dbd3574ee39a26ab77483a2be7f7e3e75fcd
Eleanor Young <developer@nausicaea.net>
2025-09-02 07:58:41 +0200

    chore: add a build script because composite project editing in Gradle/Loom drives me crazy

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rm -rf special-models/build special-models/.gradle limlib/build limlib/.gradle build .gradle $HOME/.gradle
+cd special-models; ./gradlew --no-daemon --quiet :clean :build; cd -; 
+cd limlib; ./gradlew --no-daemon --quiet :build; cd -; 
+./gradlew --no-daemon --quiet :build
